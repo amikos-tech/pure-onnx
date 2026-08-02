17c29b80f1ad7b22244079a7773dbcafdd0ae273
Trayan Azarov <trayan.azarov@amikos.tech>
2026-08-02 11:08:14 +0300

    docs(openclip): clarify EmbedDocuments/EmbedQuery are Embedder[T] aliases

--- embeddings/openclip/generalized_embedder.go
+++ embeddings/openclip/generalized_embedder.go
@@ -1,11 +1,11 @@
 package openclip
 
-// EmbedDocuments forwards document-shaped text input to EmbedTexts.
+// EmbedDocuments is an alias for EmbedTexts, added solely to satisfy embeddings.Embedder[T].
 func (e *Embedder) EmbedDocuments(documents []string) ([][]float32, error) {
 	return e.EmbedTexts(documents)
 }
 
-// EmbedQuery forwards query-shaped text input to EmbedText.
+// EmbedQuery is an alias for EmbedText, added solely to satisfy embeddings.Embedder[T].
 func (e *Embedder) EmbedQuery(query string) ([]float32, error) {
 	return e.EmbedText(query)
 }
